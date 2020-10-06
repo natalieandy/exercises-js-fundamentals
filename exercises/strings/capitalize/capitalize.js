1a03d27b15c5892e49dc4c052aaba2f571ad273f
Natalie Anderson <natalie.anderson6@gmail.com>
2020-10-06 10:08:33 -0700

    Completed capitalize

--- exercises/strings/capitalize/capitalize.js
+++ exercises/strings/capitalize/capitalize.js
@@ -6,16 +6,22 @@
  *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
  *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice
  *
+ * Referenced: https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+ *
  * @param {string} string - The string to capitalized
  * @returns {string} A capitalized copy of the string
  */
 
 function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
   // This is your job. :)
 }
 
 if (require.main === module) {
-  console.log('Running sanity checks for capitalize:');
+  console.log(capitalize('running sanity checks for capitalize:'));
+  console.log(capitalize('natalie'));
+  console.log(capitalize('Penny'));
+  console.log(capitalize('hello'));
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
