2b14af9563f7637536104e680708f4945d94064c
Natalie Anderson <natalie.anderson6@gmail.com>
2020-10-05 18:03:19 -0700

    Completed celsiusToFahrenheit

--- exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
+++ exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
@@ -4,12 +4,22 @@
  * @param {number} temp - The temperature in Celsius
  * @returns {boolean} The temperature in Fahrenheit
  */
+
+ // Why will it return a boolean? Isn't it returning a number ?^^^
+
 function celsiusToFahrenheit(temp) {
+  return temp * 9/5 + 32
   // This is your job. :).
 }
 
 if (require.main === module) {
+
   console.log('Running sanity checks for celsiusToFahrenheit:');
+
+  console.log(celsiusToFahrenheit(5));
+  console.log(celsiusToFahrenheit(9));
+  console.log(celsiusToFahrenheit(35));
+  console.log(celsiusToFahrenheit(8));
 
   // Your sanity checks go here.
 }
