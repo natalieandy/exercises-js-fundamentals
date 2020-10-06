1dc9cbdd3124c9b6804980478f80d2901a80671a
Natalie Anderson <natalie.anderson6@gmail.com>
2020-10-06 10:47:20 -0700

    Completed shortestWord

--- exercises/strings/shortestWord/shortestWord.js
+++ exercises/strings/shortestWord/shortestWord.js
@@ -8,11 +8,23 @@
  */
 
 function shortestWord(string) {
+    let stringArray = string.split(" ");
+    let shortestWord = stringArray.reduce((a, b) => {
+      if(b.length < a.length) {
+        return b;
+      } else {
+        return a;
+      }
+    });
+    return shortestWord;
   // This is your job. :)
 }
 
 if (require.main === module) {
-  console.log('Running sanity checks for shortestWord:');
+  console.log(shortestWord('Running sanity checks for shortestWord:'));
+  console.log(shortestWord('10 more fundamentals to go until I have reached my goal for today'));
+  console.log(shortestWord('She sells sea shells by the sea shore'));
+  console.log(shortestWord('This City by Sam Fischer is a good but sad song'));
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
