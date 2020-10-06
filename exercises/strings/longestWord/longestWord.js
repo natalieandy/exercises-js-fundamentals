a7341b1d4e08e6661c0b9b82842a1aa01eb0cabb
Natalie Anderson <natalie.anderson6@gmail.com>
2020-10-06 10:40:20 -0700

    Completed longestWord

--- exercises/strings/longestWord/longestWord.js
+++ exercises/strings/longestWord/longestWord.js
@@ -7,12 +7,28 @@
  * @returns {string} The longest word in the input string
  */
 
+ // Referenced: https://codeburst.io/how-to-find-the-longest-word-in-a-string-using-javascript-2fb817f78eed
+
 function longestWord(string) {
+  let stringArray = string.split(" ");
+  let longestWord = stringArray.reduce((a, b) => {
+    if(b.length > a.length) {
+      return b;
+    } else {
+      return a;
+    }
+  });
+  return longestWord;
   // This is your job. :)
 }
 
 if (require.main === module) {
-  console.log('Running sanity checks for longestWord:');
+  console.log(longestWord('Hello world how are you!'));
+  console.log(longestWord('Coding is c00l'));
+  console.log(longestWord('I hope this works'));
+  console.log(longestWord('Listening to music while coding is v relaxing'));
+
+
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
